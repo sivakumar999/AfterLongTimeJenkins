ea0a5cdcfbb2b1d2089825e5f2c624b29b5f6cfd
sivakumar999 <sivakumar59498@gmail.com>
2023-09-14 11:29:18 +0530

    Done

---
+++ WebApp/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<user> users = new List<user>
+        {
+            new user(){UserId = 1,UserName ="Sam",UserPassword="Sam@1256",UserEmail="same5@gamil.com"},
+            new user(){UserId = 2,UserName ="wan",UserPassword="wan@1256",UserEmail="wan@gamil.com"},
+            new user(){UserId = 3,UserName ="lan",UserPassword="lan@1256",UserEmail="lan@gamil.com"},
+            new user(){UserId = 4,UserName ="man",UserPassword="man@1256",UserEmail="man@gamil.com"},
+        };
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new user());
+        }
+
+        [HttpPost]
+        public IActionResult Create(user model)
+        {
+            if (ModelState.IsValid)
+            {
+                users.Add(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WebApp/Models/user.cs
+++ WebApp/Models/user.cs
@@ -1,10 +1,18 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class user
     {
+        [Required]
         public int UserId { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string UserPassword { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string UserEmail { get; set; }
     }
 }
